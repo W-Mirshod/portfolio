e01b4224a65a3a45d82ab56654406e4430b4941d
Mirshod <python3526@gmail.com>
2024-09-03 11:31:03 +0500

    minor bug;

--- static/script.js
+++ static/script.js
@@ -226,11 +226,3 @@ $(document).ready(function () {
         });
     });
 });
-
-window.addEventListener('load', function() {
-    var audio = document.getElementById('background-music');
-    audio.play().catch(function(error) {
-        console.log('Playback failed:', error);
-    });
-});
-
