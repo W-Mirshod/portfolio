7bf6e971dec5bf056619e12a9d13ef2b08d2dcee
Mirshod <python3526@gmail.com>
2024-09-03 10:15:40 +0500

    admin-page

--- root/urls.py
+++ root/urls.py
@@ -21,6 +21,6 @@ from django.urls import path, include
 from root import settings
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
+                  path('admin-page/', admin.site.urls),
                   path('', include('portfolio.urls'))
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
