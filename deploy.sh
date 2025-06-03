835400abe04f590d3b37a8437c03850752e863cc
Mirshod <python3526@gmail.com>
2025-06-04 02:19:14 +0500

    Add deployment workflow and script for automated deployment

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Navigate to the project directory
+cd /mnt/portfolio
+
+# Fetch the latest changes from the repository
+git fetch origin
+git reset --hard origin/master
+git clean -fd
+
+# Rebuild and restart the Docker containers
+docker-compose up --build -d
