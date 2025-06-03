e68d8385fc58382397aa4bd5ded9e5097601fd23
Mirshod <python3526@gmail.com>
2025-06-04 01:38:23 +0500

    Add new CSS styles for v2 layout and animations
    - Implemented a comprehensive stylesheet for the v2 design.
    - Set global styles for body, headers, and buttons.
    - Created a responsive header and navigation menu with hover effects.
    - Designed hero section with left and right alignment, including text and button styles.
    - Added social media icons with hover animations.
    - Included keyframe animations for fade-in effects on various elements.
    - Enhanced background with a zoom animation for a dynamic visual experience.

--- root/urls.py
+++ root/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
                   path('admin-page/', admin.site.urls),
                   path('', include('portfolio.urls')),
                   path('v2', include('v2.urls'))
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
