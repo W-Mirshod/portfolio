eef96ffd45df1bb30f33c82020542922e1c0e540
Mirshod <python3526@gmail.com>
2025-06-04 14:09:07 +0500

    Refactor deployment process by moving git commands from deploy.sh to deploy.yml for improved workflow efficiency

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,5 @@
 # Navigate to the project directory
 cd /mnt/portfolio
 
-# Fetch the latest changes from the repository
-git fetch origin
-git reset --hard origin/master
-git clean -fd
-git pull
-
 # Rebuild and restart the Docker containers 
 docker-compose up --build -d
