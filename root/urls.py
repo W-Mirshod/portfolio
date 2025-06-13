a58ba4abbd8a1f856109d0fb2f36a07a2209cebd
Mirshod <python3526@gmail.com>
2025-06-13 07:24:24 +0500

    Enhance security settings by enabling secure cookies for production and remove CSRF exemption from language setting view

--- root/urls.py
+++ root/urls.py
@@ -6,13 +6,12 @@ from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import path, include
-from django.views.decorators.csrf import csrf_exempt
 from django.views.i18n import set_language
 from portfolio.sitemaps import sitemaps
 from root import settings
 
 urlpatterns = [
-    path('i18n/setlang/', csrf_exempt(set_language), name='set_language'),
+    path('i18n/setlang/', set_language, name='set_language'),
     path('i18n/', include('django.conf.urls.i18n')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 ]
@@ -20,5 +19,8 @@ urlpatterns = [
 urlpatterns += i18n_patterns(
     path('admin-page/', admin.site.urls),
     path('', include('portfolio.urls')),
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
-  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+)
+
+# Static and media should not be inside i18n_patterns in production
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
